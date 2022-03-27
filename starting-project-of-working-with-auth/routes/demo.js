92582490b1595193e45b8ff745dd2cc7f372b112
AhmadHaleeem <coldman2099@gmail.com>
2022-03-27 12:31:07 +0200

    end working with auth system

--- starting-project-of-working-with-auth/routes/demo.js
+++ starting-project-of-working-with-auth/routes/demo.js
@@ -10,11 +10,34 @@ router.get('/', function (req, res) {
 });
 
 router.get('/signup', function (req, res) {
-  res.render('signup');
+  let sessionInputData = req.session.userData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      confirmEmail: "",
+      password: ""
+    }
+  }
+
+  req.session.userData = null;
+
+  res.render('signup', { inputData: sessionInputData });
 });
 
 router.get('/login', function (req, res) {
-  res.render('login');
+  let sessionInputData = req.session.userData;
+  if (!sessionInputData) {
+    sessionInputData = {
+      hasError: false,
+      email: "",
+      password: ""
+    }
+  }
+
+  req.session.userData = null;
+
+  res.render('login', { inputData: sessionInputData });
 });
 
 router.post('/signup', async function (req, res) {
@@ -27,14 +50,35 @@ router.post('/signup', async function (req, res) {
     || enteredPassword.trim() < 6
     || enteredEmail !== enteredConfirmEmail || !enteredEmail.includes('@')
   ) {
-    console.log('Incorrect Data');
-    return res.redirect("/signup")
+    //console.log('Incorrect Data');
+    req.session.userData = {
+      hasError: true,
+      message: "Invalid Input - please check your data",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    }
+    
+    req.session.save(function() {
+      res.redirect("/signup")
+    })
+    return;
   }
 
   const existingUser = await db.getDb().collection("users").findOne({email: enteredEmail});
   if (existingUser) {
-    console.log('User exists already..');
-    return res.redirect("/signup")
+    req.session.userData = {
+      hasError: true,
+      message: "User exists already!",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword
+    }
+
+    req.session.save(function() {
+      res.redirect("/signup")
+    })
+    return;
   }
 
   const hashedPassword = await bcrypt.hash(enteredPassword, 12)
@@ -59,25 +103,71 @@ router.post('/login', async function (req, res) {
     .findOne({ email: enteredEmail })
 
   if (!existingUser) {
-    console.log('Could not log in!');
-    return res.redirect("/login");
+    req.session.userData = {
+      hasError: true,
+      message: "Could not log you in - please check your credentails",
+      email: enteredEmail,
+      password: enteredPassword
+    }
+
+    req.session.save(function() {
+      res.redirect("/login")
+    })
+    return;
   }
 
   const passwordsAreEqual = await bcrypt.compare(enteredPassword, existingUser.password);
 
   if (!passwordsAreEqual) {
-    console.log('Could not log in! - password is not correct!');
-    return res.redirect("/login");
+    req.session.userData = {
+      hasError: true,
+      message: "Could not log in! - password is not correct!",
+      email: enteredEmail,
+      password: enteredPassword
+    }
+
+    req.session.save(function() {
+      res.redirect("/login")
+    })
+    return;
   }
 
-  console.log('User is authenticated..');
-  res.redirect("/admin")
+  //console.log('User is authenticated..');
+  req.session.user = { id: existingUser._id, email: existingUser.email };
+  req.session.isAuthenticated = true;
+  req.session.save(function() {
+    res.redirect("/profile");
+  });
+
 });
 
-router.get('/admin', function (req, res) {
+router.get('/admin', async function (req, res) {
+  // Check the user 'ticket'
+  if (!res.locals.isAuth) { // if (!req.session.user)
+    return res.status(401).render("401");
+  }
+
+  //const user = await db.getDb().collection("users").findOne({_id: req.session.user.id});
+
+  if (!res.locals.isAdmin) {
+    return res.status(403).render("403")
+  }
+
   res.render('admin');
 });
 
-router.post('/logout', function (req, res) { });
+router.get('/profile', function (req, res) {
+  // Check the user 'ticket'
+  if (!res.locals.isAuth) { // if (!req.session.user)
+    return res.status(401).render("401");
+  }
+  res.render('profile');
+});
+
+router.post('/logout', function (req, res) {
+  req.session.user = null;
+  req.session.isAuthenticated = false;
+  res.redirect("/");
+});
 
 module.exports = router;
